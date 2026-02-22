46266e1fa39110d49f9091f2e3439fc2394c814e
qassem abied <qassemab1@gmail.com>
2026-02-23 00:38:04 +0200

    Centralized color definitions for the entire application.

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+abstract class AppColors {
+  static const Color primaryColorDark = Color(0xFF15B86C);
+  static const Color primaryTextDark = Color(0xFFFFFCFC);
+  static const Color secondaryTextDark = Color(0xFFC6C6C6);
+  static const Color tertiaryTextDark = Color(0xFFA0A0A0);
+  static const Color surfaceDark = Color(0xFF282828);
+  static const Color borderDark = Color(0xFF6E6E6E);
+  static const Color bgColorDark = Color(0xFF181818);
+
+  static const Color primaryColor = Color(0xFF15B86C);
+  static const Color primaryTextLight = Color(0xFF161F1B);
+  static const Color secondaryTextLight = Color(0xFF3A4640);
+  static const Color tertiaryTextLight = Color(0xFF6A6A6A);
+  static const Color surfaceLight = Color(0xFFFFFFFF);
+  static const Color borderLight = Color(0xFFD1DAD6);
+  static const Color bgColorLight = Color(0xFFF6F7F9);
+}
