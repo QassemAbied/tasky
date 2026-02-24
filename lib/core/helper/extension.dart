f46572feacb636c3b300048e177bfe7379990bc7
qassem abied <qassemab1@gmail.com>
2026-02-24 05:29:32 +0200

    refactor code

--- lib/core/helper/extension.dart
+++ lib/core/helper/extension.dart
@@ -2,13 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:tasky/core/theme/app_colors.dart';
 
 extension TextColorExtension on BuildContext {
-  Color get textPrimary => Theme.of(this).brightness == Brightness.dark
-      ? AppColors.primaryTextDark
-      : AppColors.primaryTextLight;
-  Color get textSecondary => Theme.of(this).brightness == Brightness.dark
-      ? AppColors.secondaryTextDark
-      : AppColors.secondaryTextLight;
-  Color get textTertiary => Theme.of(this).brightness == Brightness.dark
-      ? AppColors.tertiaryTextDark
-      : AppColors.tertiaryTextLight;
+  bool get isDark => Theme.of(this).brightness == Brightness.dark;
+  Color get textPrimary =>
+      isDark ?
+      AppColors.primaryTextDark : AppColors.primaryTextLight;
+  Color get textSecondary =>
+      isDark ?
+      AppColors.secondaryTextDark : AppColors.secondaryTextLight;
+  Color get textTertiary =>
+      isDark ?
+      AppColors.tertiaryTextDark : AppColors.tertiaryTextLight;
+  Color get textPlaceholder =>
+      isDark ?
+      AppColors.placeholderTextDark : AppColors.placeholderTextLight;
+  Color get textBorder => isDark ? AppColors.borderDark : AppColors.borderLight;
+  Color get textSurface => isDark ? AppColors.surfaceDark : AppColors.surfaceLight;
+
 }
