f92b170605193f4aa2a9720162cfdb8cb94a4aaf
qassem abied <qassemab1@gmail.com>
2026-02-28 22:39:58 +0200

    refactor shared pref

--- lib/core/helper/shared_pref.dart
+++ lib/core/helper/shared_pref.dart
@@ -1,8 +1,7 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPrefHelper {
-  SharedPrefHelper ._();
-
+  SharedPrefHelper._();
 
   static late SharedPreferences _pref;
 
@@ -10,34 +9,44 @@ class SharedPrefHelper {
     _pref = await SharedPreferences.getInstance();
   }
 
-  static Future<bool> setData({required String key, required dynamic value} )async{
+  static Future<bool> setData({
+    required String key,
+    required dynamic value,
+  }) async {
     if (value is String) return _pref.setString(key, value);
     if (value is int) return _pref.setInt(key, value);
     if (value is bool) return _pref.setBool(key, value);
     if (value is double) return _pref.setDouble(key, value);
+    if (value is List<String>) return _pref.setStringList(key, value);
 
     return false;
   }
 
-  static int? getInt({required String key}){
-    return _pref.getInt(key)??0;
+  static int? getInt({required String key}) {
+    return _pref.getInt(key) ?? 0;
   }
-  static bool? getBool({required String key}){
+
+  static List<String>? getList({required String key}) {
+    return _pref.getStringList(key) ?? [];
+  }
+
+  static bool? getBool({required String key}) {
     return _pref.getBool(key);
   }
-  static double? getDouble({required String key})  {
+
+  static double? getDouble({required String key}) {
     return _pref.getDouble(key);
   }
-  static String? getString({required String key})  {
-    return _pref.getString(key) ;
+
+  static String? getString({required String key}) {
+    return _pref.getString(key);
   }
 
-
-  static Future<bool> removeData(String key)  {
-  return   _pref.remove(key);
+  static Future<bool> removeData(String key) {
+    return _pref.remove(key);
   }
 
-  static Future<bool> clearAllData()  {
-  return   _pref.clear();
+  static Future<bool> clearAllData() {
+    return _pref.clear();
   }
 }
