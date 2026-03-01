19053c39d934aa071e824d0459e9a3bd7e322764
qassem abied <qassemab1@gmail.com>
2026-03-02 00:31:12 +0200

    added todo feature in home cubit and states

--- lib/feature/presentation/controller/home_cubit/home_cubit.dart
+++ lib/feature/presentation/controller/home_cubit/home_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tasky/feature/domain/usecases/get_task_usecase.dart';
 import '../../../domain/entities/task_entities.dart';
@@ -8,7 +10,9 @@ class HomeCubit extends Cubit<HomeState> {
   final GetTaskUseCase getTasksUseCase;
   final UpdateTaskUseCase _updateTaskUseCase;
   HomeCubit(this.getTasksUseCase, this._updateTaskUseCase)
-    : super(const HomeState());
+    : super(const HomeState()){
+    loadData();
+  }
 
   Future<void> toggleMyTask(TaskEntities task, bool value) async {
     final updatedTasks = List<TaskEntities>.from(state.tasks);
@@ -27,25 +31,113 @@ class HomeCubit extends Cubit<HomeState> {
       isDone: value,
     );
 
-    emit(state.copyWith(tasks: updatedTasks));
+    emit(state.copyWith(
+      tasks: updatedTasks,
+
+      highPriorityTasks:
+      updatedTasks.where((e) => e.highPriority).toList(),
+
+      noHighPriorityTasks:
+      updatedTasks.where((e) => !e.highPriority).toList(),
+        todoTask: updatedTasks.where((e)=>!e.isDone).toList(),
+    ));
     await _updateTaskUseCase.call(updatedTasks);
   }
 
-  Future<void> getTasks() async {
-    emit(state.copyWith(status: HomeStatus.loading));
-
+  Future<void> loadData() async {
+    emit(state.copyWith(
+      status: HomeStatus.loading,
+      todoStatus: TodoStatus.loading,
+    ));
     try {
-      final tasks = await getTasksUseCase();
 
-      emit(state.copyWith(status: HomeStatus.loaded));
+      final tasks = await getTasksUseCase();
+      emit(state.copyWith(
+
+        highPriority: HighPriority.loaded,
+        noHighPriority: NoHighPriority.loaded,
+          status: HomeStatus.loaded,
+        todoStatus: TodoStatus.loaded,
+        tasks: tasks,
+        highPriorityTasks: tasks.where((e) => e.highPriority).toList(),
+        noHighPriorityTasks: tasks.where((e) => !e.highPriority).toList(),
+        todoTask: tasks.where((e)=>!e.isDone).toList()
+      ));
     } catch (e) {
-      emit(state.copyWith(status: HomeStatus.error, error: e.toString()));
+      emit(state.copyWith(
+        status: HomeStatus.error,
+        highPriority: HighPriority.error,
+        noHighPriority: NoHighPriority.error,
+        todoStatus: TodoStatus.error,
+        error: e.toString(),
+      ));
     }
   }
+  // Future<void> getAllTasks() async {
+  //   emit(state.copyWith(status: HomeStatus.loading));
+  //
+  //   try {
+  //     final tasks = await getTasksUseCase();
+  //
+  //     emit(
+  //         state.copyWith(
+  //             status: HomeStatus.loaded,
+  //             tasks: tasks));
+  //
+  //   } catch (e) {
+  //     emit(state.copyWith(status: HomeStatus.error, error: e.toString()));
+  //   }
+  // }
 
-  List<TaskEntities> get highPriorityTasks =>
-      state.tasks.where((task) => task.highPriority).toList();
+  // Future<void> getHighPriorityTasks() async {
+  //   emit(state.copyWith(highPriority: HighPriority.loading));
+  //
+  //   try {
+  //     final tasks = await getTasksUseCase();
+  //
+  //     emit(
+  //         state.copyWith(
+  //             highPriority: HighPriority.loaded,
+  //             highPriorityTasks: tasks.where((task) => task.highPriority==true).toList()));
+  //
+  //   } catch (e) {
+  //     emit(state.copyWith(highPriority: HighPriority.error, error: e.toString()));
+  //   }
+  // }
+  // // List<TaskEntities> get highPriorityTasks =>
+  // //     state.tasks.where((task) => task.highPriority).toList();
+  //
+  // Future<void> getNoHighPriorityTasks() async {
+  //   emit(state.copyWith(noHighPriority: NoHighPriority.loading));
+  //
+  //   try {
+  //     final tasks = await getTasksUseCase();
+  //
+  //     emit(
+  //         state.copyWith(
+  //             noHighPriority: NoHighPriority.loaded,
+  //             noHighPriorityTasks: tasks.where((e) => e.highPriority == false).toList()));
+  //
+  //   } catch (e) {
+  //     emit(state.copyWith(noHighPriority: NoHighPriority.error, error: e.toString()));
+  //   }
+  // }
+  // // List<TaskEntities> get noHighPriorityTasks =>
+  // //     state.tasks.where((e) => e.highPriority == false).toList();
+  //
+  //
+  // Future<void> getNoCompletedTask()async{
+  //   emit(state.copyWith(status: HomeStatus.loading));
+  //
+  //   try {
+  //     final tasks = await getTasksUseCase();
+  //
+  //     emit(state.copyWith(status: HomeStatus.loaded,
+  //         tasks: tasks.where((e) => e.isDone==true ).toList()));
+  //
+  //   } catch (e) {
+  //     emit(state.copyWith(status: HomeStatus.error, error: e.toString()));
+  //   }
+  // }
 
-  List<TaskEntities> get noHighPriorityTasks =>
-      state.tasks.where((e) => e.highPriority == false).toList();
 }
--- lib/feature/presentation/controller/home_cubit/home_state.dart
+++ lib/feature/presentation/controller/home_cubit/home_state.dart
@@ -2,29 +2,55 @@ import '../../../domain/entities/task_entities.dart';
 
 enum HomeStatus { initial, loading, loaded, error }
 
+enum HighPriority { initial, loading, loaded, error }
+
+enum NoHighPriority { initial, loading, loaded, error }
+enum TodoStatus { initial, loading, loaded, error }
+
 class HomeState {
   final HomeStatus status;
+  final HighPriority highPriority;
+  final NoHighPriority noHighPriority;
+
   final List<TaskEntities> tasks;
+  final List<TaskEntities> highPriorityTasks;
+  final List<TaskEntities> noHighPriorityTasks;
   final String? error;
-
-
+  final TodoStatus todoStatus;
+  final List<TaskEntities> todoTask;
   const HomeState({
     this.status = HomeStatus.initial,
+    this.highPriority = HighPriority.initial,
+    this.noHighPriority = NoHighPriority.initial,
+    this.todoStatus = TodoStatus.initial,
+    this.todoTask = const [],
     this.tasks = const [],
+    this.noHighPriorityTasks = const [],
+    this.highPriorityTasks = const [],
     this.error,
   });
 
   HomeState copyWith({
     HomeStatus? status,
+    HighPriority? highPriority,
+    NoHighPriority? noHighPriority,
     List<TaskEntities>? tasks,
+     List<TaskEntities>? highPriorityTasks,
+     List<TaskEntities>? noHighPriorityTasks,
     String? error,
-
+    TodoStatus? todoStatus,
+    List<TaskEntities>? todoTask,
   }) {
     return HomeState(
       status: status ?? this.status,
+      highPriority: highPriority ?? this.highPriority,
+      noHighPriority: noHighPriority ?? this.noHighPriority,
       tasks: tasks ?? this.tasks,
+      highPriorityTasks: highPriorityTasks ?? this.highPriorityTasks,
+      noHighPriorityTasks: noHighPriorityTasks ?? this.noHighPriorityTasks,
       error: error ?? this.error,
-
+      todoStatus: todoStatus ?? this.todoStatus,
+      todoTask: todoTask ?? this.todoTask,
     );
   }
 }
