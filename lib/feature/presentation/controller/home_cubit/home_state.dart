b3199fbfcf283843dfdedf1d110df40714e7435c
qassem abied <qassemab1@gmail.com>
2026-03-02 00:36:40 +0200

    added states in completed task feature

--- lib/feature/presentation/controller/home_cubit/home_state.dart
+++ lib/feature/presentation/controller/home_cubit/home_state.dart
@@ -5,28 +5,35 @@ enum HomeStatus { initial, loading, loaded, error }
 enum HighPriority { initial, loading, loaded, error }
 
 enum NoHighPriority { initial, loading, loaded, error }
+
 enum TodoStatus { initial, loading, loaded, error }
+
+enum CompletedStatus { initial, loading, loaded, error }
 
 class HomeState {
   final HomeStatus status;
   final HighPriority highPriority;
   final NoHighPriority noHighPriority;
-
+  final TodoStatus todoStatus;
+  final CompletedStatus completedStatus;
   final List<TaskEntities> tasks;
   final List<TaskEntities> highPriorityTasks;
   final List<TaskEntities> noHighPriorityTasks;
-  final String? error;
-  final TodoStatus todoStatus;
   final List<TaskEntities> todoTask;
+  final List<TaskEntities> completedTask;
+  final String? error;
+
   const HomeState({
     this.status = HomeStatus.initial,
     this.highPriority = HighPriority.initial,
     this.noHighPriority = NoHighPriority.initial,
     this.todoStatus = TodoStatus.initial,
+    this.completedStatus = CompletedStatus.initial,
     this.todoTask = const [],
     this.tasks = const [],
     this.noHighPriorityTasks = const [],
     this.highPriorityTasks = const [],
+    this.completedTask = const [],
     this.error,
   });
 
@@ -34,23 +41,27 @@ class HomeState {
     HomeStatus? status,
     HighPriority? highPriority,
     NoHighPriority? noHighPriority,
-    List<TaskEntities>? tasks,
-     List<TaskEntities>? highPriorityTasks,
-     List<TaskEntities>? noHighPriorityTasks,
-    String? error,
     TodoStatus? todoStatus,
+    CompletedStatus? completedStatus,
+    List<TaskEntities>? tasks,
+    List<TaskEntities>? highPriorityTasks,
+    List<TaskEntities>? noHighPriorityTasks,
     List<TaskEntities>? todoTask,
+    List<TaskEntities>? completedTask,
+    String? error,
   }) {
     return HomeState(
       status: status ?? this.status,
       highPriority: highPriority ?? this.highPriority,
       noHighPriority: noHighPriority ?? this.noHighPriority,
+      todoStatus: todoStatus ?? this.todoStatus,
+      completedStatus: completedStatus ?? this.completedStatus,
       tasks: tasks ?? this.tasks,
       highPriorityTasks: highPriorityTasks ?? this.highPriorityTasks,
       noHighPriorityTasks: noHighPriorityTasks ?? this.noHighPriorityTasks,
-      error: error ?? this.error,
-      todoStatus: todoStatus ?? this.todoStatus,
       todoTask: todoTask ?? this.todoTask,
+      completedTask: completedTask ?? this.completedTask,
+      error: error ?? this.error,
     );
   }
 }
