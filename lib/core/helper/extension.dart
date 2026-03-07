ba4b39d757ad5d743ba8cc3bbcdb24e112402a74
qassem abied <qassemab1@gmail.com>
2026-03-08 01:52:37 +0200

    added navigation extension

--- lib/core/helper/extension.dart
+++ lib/core/helper/extension.dart
@@ -19,3 +19,30 @@ extension TextColorExtension on BuildContext {
   Color get textSurface => isDark ? AppColors.surfaceDark : AppColors.surfaceLight;
 
 }
+extension Navigation on BuildContext {
+  Future<dynamic> pushNamed(String routeName, {Object? arguments}) {
+    return Navigator.of(this).pushNamed(routeName, arguments: arguments);
+  }
+  Future<dynamic> push(Route<dynamic> routeName, ) {
+    return Navigator.of(this).push(routeName,);
+  }
+
+  Future<dynamic> pushReplacementNamed(String routeName, {Object? arguments}) {
+    return Navigator.of(
+      this,
+    ).pushReplacementNamed(routeName, arguments: arguments);
+  }
+
+  void pop() {
+    if (Navigator.of(this).canPop()) {
+      Navigator.of(this).pop();
+    }
+  }
+
+  void popUntil(String routeName) {
+    Navigator.of(this).popUntil((route) => route.settings.name == routeName);
+  }
+
+
+  ColorScheme get colors => Theme.of(this).colorScheme;
+}
