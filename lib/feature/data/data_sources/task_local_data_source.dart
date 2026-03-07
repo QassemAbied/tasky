2fff8b13bb9019313bdeb185bdc793fa4bff490d
qassem abied <qassemab1@gmail.com>
2026-03-08 01:55:12 +0200

    implement logout function in data source

--- lib/feature/data/data_sources/task_local_data_source.dart
+++ lib/feature/data/data_sources/task_local_data_source.dart
@@ -1,7 +1,16 @@
+import 'dart:ffi';
+
 import 'package:tasky/feature/data/model/task_model.dart';
 
 abstract class TaskLocalDataSource {
   Future<TaskModel> addTask(TaskModel task);
   Future<List<TaskModel>> getTask();
   Future<void> updateTask(List<TaskModel> task);
+  Future addUserName(String userName);
+  String? getUserName();
+
+
+  Future addQuoteUser(String quote);
+  String? getQuoteUser();
+  Future logOut();
 }
--- lib/feature/data/data_sources/task_local_data_source_impl.dart
+++ lib/feature/data/data_sources/task_local_data_source_impl.dart
@@ -5,8 +5,6 @@ import 'package:tasky/core/helper/shared_pref.dart';
 import 'package:tasky/feature/data/data_sources/task_local_data_source.dart';
 import 'package:tasky/feature/data/model/task_model.dart';
 
-import '../../domain/entities/task_entities.dart';
-
 class TaskLocalDataSourceImpl implements TaskLocalDataSource {
   @override
   Future<TaskModel> addTask(TaskModel task) async {
@@ -25,27 +23,55 @@ class TaskLocalDataSourceImpl implements TaskLocalDataSource {
   }
 
   @override
-  Future<List<TaskModel>> getTask() async{
-  final getTasks= SharedPrefHelper.getString(key: AppConstants.addTaskKey);
-  if(getTasks !=null) {
-    final taskDecode= jsonDecode(getTasks)  ;
-    return taskDecode.map<TaskModel>((e)=>TaskModel.fromJson(e)).toList();
-  }
- return [];
-
+  Future<List<TaskModel>> getTask() async {
+    final getTasks = SharedPrefHelper.getString(key: AppConstants.addTaskKey);
+    if (getTasks != null) {
+      final taskDecode = jsonDecode(getTasks);
+      return taskDecode.map<TaskModel>((e) => TaskModel.fromJson(e)).toList();
+    }
+    return [];
   }
 
   @override
-  Future<void> updateTask(List<TaskModel> task)async {
-
-    final data= task.map((e)=>e.toJson()).toList();
+  Future<void> updateTask(List<TaskModel> task) async {
+    final data = task.map((e) => e.toJson()).toList();
     await SharedPrefHelper.setData(
       key: AppConstants.addTaskKey,
       value: jsonEncode(data),
     );
-
-
   }
 
+  @override
+  String? getUserName() {
+    return SharedPrefHelper.getString(key: AppConstants.userNameKey);
+  }
 
+  @override
+  Future addUserName(String userName) async {
+    return await SharedPrefHelper.setData(
+      key: AppConstants.userNameKey,
+      value: userName,
+    );
+  }
+
+  @override
+  Future<dynamic> addQuoteUser(String quote) async {
+    return await SharedPrefHelper.setData(
+      key: AppConstants.quoteKey,
+      value: quote,
+    );
+  }
+
+  @override
+  String? getQuoteUser() {
+    return SharedPrefHelper.getString(key: AppConstants.quoteKey);
+  }
+
+  @override
+  Future<dynamic> logOut()async {
+    await SharedPrefHelper.removeData(AppConstants.addTaskKey);
+    await SharedPrefHelper.removeData(AppConstants.onBoardingKey);
+    await SharedPrefHelper.removeData(AppConstants.quoteKey);
+    await SharedPrefHelper.removeData(AppConstants.userNameKey);
+  }
 }
