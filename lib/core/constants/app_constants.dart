c03dad1ba71208e274863828e6c21c4a4703ca7f
qassem abied <qassemab1@gmail.com>
2026-03-08 01:51:18 +0200

    added some keys

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,5 +1,8 @@
 class AppConstants {
   AppConstants._();
-  static const String nameKey = 'name';
+  static const String userNameKey = 'name';
+  static const String quoteKey = 'quote';
   static const String themeModeKey = 'themeMode';
+  static const String onBoardingKey = 'onBoarding';
+  static const String addTaskKey = 'addTask';
 }
