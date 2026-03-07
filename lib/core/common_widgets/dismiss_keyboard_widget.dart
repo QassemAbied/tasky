081d2391e629235ab7527aa9dfadb86e9dc9ceff
qassem abied <qassemab1@gmail.com>
2026-03-08 01:49:21 +0200

    implement genrel function to handle keyboard

---
+++ lib/core/common_widgets/dismiss_keyboard_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DismissKeyboardWidget extends StatelessWidget {
+  final Widget child;
+
+  const DismissKeyboardWidget({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: child,
+    );
+  }
+}
