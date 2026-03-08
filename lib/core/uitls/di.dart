41a887cacd550407f89f29ea4d3b7f9f7e493fe6
qassem abied <qassemab1@gmail.com>
2026-03-08 02:09:48 +0200

    added d i

--- lib/core/uitls/di.dart
+++ lib/core/uitls/di.dart
@@ -1,8 +1,14 @@
 import 'package:get_it/get_it.dart';
 import 'package:tasky/feature/domain/repositories/add_task_repos.dart';
+import 'package:tasky/feature/domain/usecases/add_quote_usecase.dart';
+import 'package:tasky/feature/domain/usecases/add_user_name_use_case.dart';
+import 'package:tasky/feature/domain/usecases/get_quote_usecase.dart';
 import 'package:tasky/feature/domain/usecases/get_task_usecase.dart';
+import 'package:tasky/feature/domain/usecases/get_user_name_usecase.dart';
+import 'package:tasky/feature/domain/usecases/logout_usecase.dart';
 import 'package:tasky/feature/domain/usecases/update_task_usecase.dart';
 import 'package:tasky/feature/presentation/controller/add_task_cubit/add_new_task_cubit.dart';
+import 'package:tasky/feature/presentation/controller/user_details/user_details_cubit.dart';
 import '../../feature/data/data_sources/task_local_data_source.dart';
 import '../../feature/data/data_sources/task_local_data_source_impl.dart';
 import '../../feature/data/repositories_impl/add_task_repos_impl.dart';
@@ -20,9 +26,15 @@ Future<void> init() async {
     () => TaskLocalDataSourceImpl(),
   );
   sl.registerLazySingleton(() => UpdateTaskUseCase(sl()));
+  sl.registerLazySingleton(() => GetUserNameUseCase(sl()));
+  sl.registerLazySingleton(() => AddUserNameUseCase(sl()));
+  sl.registerLazySingleton(() => GetQuoteUseCase(sl()));
+  sl.registerLazySingleton(() => AddQuoteUseCase(sl()));
+  sl.registerLazySingleton(() => LogoutUseCase(sl()));
   sl.registerFactory(() => AddNewTaskCubit(sl()));
 
   sl.registerLazySingleton(() => GetTaskUseCase(sl()));
 
-  sl.registerFactory(() => HomeCubit(sl(), sl()));
+  sl.registerLazySingleton(() => HomeCubit(sl(), sl()));
+  sl.registerLazySingleton(() => UserDetailsCubit(sl(), sl(),sl(), sl(),sl(),));
 }
