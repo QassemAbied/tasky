aba62171ee685a3ad0d882b22e92a0dd1c0bc679
qassem abied <qassemab1@gmail.com>
2026-03-08 02:00:41 +0200

    refactor code

--- lib/feature/presentation/pages/add_task/view/widgets/high_priority_widget.dart
+++ lib/feature/presentation/pages/add_task/view/widgets/high_priority_widget.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import '../../../../../../core/helper/extension.dart';
-import '../../../../../../core/theme/app_colors.dart';
 import '../../../../../../core/theme/app_text_style.dart';
 import '../../../../controller/add_task_cubit/add_new_task_cubit.dart';
 import '../../../../controller/add_task_cubit/add_new_task_state.dart';
@@ -23,9 +21,6 @@ class HighPriorityWidget extends StatelessWidget {
           builder: (context, state) {
             final cubit = AddNewTaskCubit.get(context);
             return Switch(
-              inactiveTrackColor: context.textSurface,
-              activeTrackColor: AppColors.primaryColor,
-              activeThumbColor: AppColors.bgColorLight,
               value: cubit.isSelected,
               onChanged: (value) {
                 cubit.toggleHighPriority(value);
