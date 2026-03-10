bbc77617b8fe9daf72faaca7e8208dbab35cd227
qassem abied <qassemab1@gmail.com>
2026-03-10 06:15:23 +0200

    added task action enum

--- lib/core/uitls/enum.dart
+++ lib/core/uitls/enum.dart
@@ -3,3 +3,13 @@ enum ThemeModeEnum{
   dark,
   light,
 }
+
+
+enum TaskItemActionsEnum{
+  mark(name: 'Mark As Done'),
+  delete(name:'Delete'),
+  edit(name:'Edit');
+
+  final String name;
+  const TaskItemActionsEnum({required this.name});
+}
