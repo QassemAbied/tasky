ea4396d48460a2439a0d8d87f10e06d18add9c2f
qassem abied <qassemab1@gmail.com>
2026-03-26 21:11:40 +0200

    implement hive init and open tow box

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:hive_ce_flutter/adapters.dart';
 import 'package:provider/provider.dart';
+import 'package:tasky/core/constants/app_constants.dart';
 import 'package:tasky/core/theme/theme_controller/theme_provider.dart';
+import 'package:tasky/feature/data/model/task_model.dart';
 import 'package:tasky/feature/presentation/controller/main_controller/main_provider.dart';
 import 'package:tasky/feature/presentation/controller/user_details_controller/user_details_provider.dart';
 import 'core/helper/shared_pref.dart';
 import 'core/uitls/di.dart';
 import 'feature/presentation/controller/home_controller/home_provider.dart';
+import 'hive_registrar.g.dart';
 import 'my_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+ await Hive.initFlutter();
+  Hive.registerAdapters();
+ await Hive.openBox<TaskModel>(AppConstants.noteCollection);
+  await Hive.openBox(AppConstants.userBox);
   await init();
   await SharedPrefHelper.init();
 
