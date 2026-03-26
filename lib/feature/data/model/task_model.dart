cd594faf318b46105ee850a94b7affb95dcb663a
qassem abied <qassemab1@gmail.com>
2026-03-26 21:08:44 +0200

    hive auto generate task model and adapter task model

--- lib/feature/data/model/task_model.dart
+++ lib/feature/data/model/task_model.dart
@@ -1,29 +1,40 @@
+
+import 'package:hive_ce_flutter/adapters.dart';
+
 import '../../domain/entities/task_entities.dart';
 
+part 'task_model.g.dart';
+
+@HiveType(typeId: 0)
 class TaskModel extends TaskEntities {
+
+  @HiveField(0)
+  final String id;
+
+  @HiveField(1)
+  final String taskName;
+
+  @HiveField(2)
+  final String taskDescription;
+
+  @HiveField(3)
+  final bool highPriority;
+
+  @HiveField(4)
+  bool isDone;
+
   TaskModel({
-    super.id,
-    required super.taskName,
-    required super.taskDescription,
-    required super.highPriority,
-    super.isDone,
-  });
+    required this.id,
+    required this.taskName,
+    required this.taskDescription,
+    required this.highPriority,
+    this.isDone = false,
+  }) : super(
+    id: id,
+    taskName: taskName,
+    taskDescription: taskDescription,
+    highPriority: highPriority,
+    isDone: isDone,
+  );
 
-  factory TaskModel.fromJson(Map<String, dynamic> json) {
-    return TaskModel(
-      id: json['id'],
-      taskName: json['taskName'],
-      taskDescription: json['taskDescription'],
-      highPriority: json['highPriority'],
-        isDone:json['isDone']??false,
-    );
-  }
-
-  Map<String, dynamic> toJson() => {
-    "id": id,
-    "taskName": taskName,
-    "taskDescription": taskDescription,
-    "highPriority": highPriority,
-    "isDone":isDone,
-  };
 }
